f4e0f6acba96968bf82d4dc56bee616ff80e7ad4
Shoxrux <ganiorzikulov@gmail.com>
2022-05-22 23:27:54 +0500

    add

---
+++ java.js
@@ -0,0 +1,81 @@
+let arr = [
+    {
+        "userId": 1,
+        "id": 1,
+        "title": "delectus aut au`tem",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 2,
+        "title": "quis ut nam facilis et officia qui",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 3,
+        "title": "fugiat veniam minus",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 4,
+        "title": "et porro tempora",
+        "completed": true
+    },
+    {
+        "userId": 1,
+        "id": 5,
+        "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 6,
+        "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 7,
+        "title": "illo expedita consequatur quia in",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 8,
+        "title": "quo adipisci enim quam ut ab",
+        "completed": true
+    },
+    {
+        "userId": 1,
+        "id": 9,
+        "title": "molestiae perspiciatis ipsa",
+        "completed": false
+    },
+    {
+        "userId": 1,
+        "id": 10,
+        "title": "illo est ratione doloremque quia maiores aut",
+        "completed": true
+    },
+]
+let a = {
+    count: 3,
+    arr: arr.splice([3], 1),
+    arr2: arr.splice([6], 1),
+    arr3: arr.splice([9], 1)
+}
+
+let b = {
+    count: 7,
+    arr: arr.splice([0], 1),
+    arr2: arr.splice([1], 1),
+    arr3: arr.splice([2], 1),
+    arr4: arr.splice([4], 1),
+    arr5: arr.splice([5], 1),
+    arr6: arr.splice([7], 1),
+    arr7: arr.splice([8], 1)
+}
+
+console.log(a, b);
